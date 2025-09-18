a858c49b9fe549b8a026aa2aacfd82ea22d3b747
SLotreck <lotrecks@msu.edu>
2025-09-18 16:19:10 -0400

    Finish final wrangling for species list

--- utils/download_sra.py
+++ utils/download_sra.py
@@ -1,5 +1,5 @@
 """
-Download files from SRA that have already been prefetched.
+Prefetch and download files from SRA.
 
 Author: Serena G. Lotreck
 """
@@ -10,16 +10,23 @@ from tqdm import tqdm
 import subprocess
 
 
-def main(prefetched_path, accession_df, outpath):
+def main(prefetch_path, accession_df, temp_loc, outpath):
 
     # Load df
     df = pd.read_csv(accession_df)
     runs_to_download = df.run_accession.tolist()
 
+    # Prefetch
+    print('Prefetching...')
+    for sra_id in tqdm(runs_to_download):
+        prefetch = "prefetch " + sra_id + " -O " + prefetch_path + "/" + sra_id
+        print('Trying to write ', prefetch_path + "/" + sra_id)
+        subprocess.call(prefetch, shell=True)
+
     # Download
     print('Downloading...')
     for sra_id in tqdm(runs_to_download):
-        fastq_dump = "fastq-dump --outdir " + outpath + " --gzip --skip-technical  --readids --read-filter pass --dumpbase --split-3 --clip " + prefetched_path + "/" + sra_id + ".sra"
+        fastq_dump = "fasterq-dump --outdir " + outpath + " -t " + temp_loc + prefetch_path + "/" + sra_id
         subprocess.call(fastq_dump, shell=True)
 
     print('\nDone!')
@@ -29,17 +36,21 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Download SRA')
 
-    parser.add_argument('prefetched_path', type=str,
-                       help='Path to the prefetched files')
+    parser.add_argument('prefetch_path', type=str,
+                       help='Path to save prefetched files')
     parser.add_argument('accession_df', type=str,
                        help='Path to the dataframe with the run numbers to download')
+    parser.add_argument('temp_loc', type=str,
+                       help='Path to put tempfiles, should be somewhere with a ton '
+                       'of space')
     parser.add_argument('outpath', type=str,
                        help='Where to save the downloaded files')
 
     args = parser.parse_args()
 
-    args.prefetched_path = abspath(args.prefetched_path)
+    args.prefetch_path = abspath(args.prefetch_path)
     args.accession_df = abspath(args.accession_df)
+    args.temp_loc = abspath(args.temp_loc)
     args.outpath = abspath(args.outpath)
 
-    main(args.prefetched_path, args.accession_df, args.outpath)
+    main(args.prefetch_path, args.accession_df, args.temp_loc, args.outpath)
